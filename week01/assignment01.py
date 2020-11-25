6ed778f8ca1c2e4a8de2ccbcc21008a4077b9a86
David Wu <toffino@aliyun.com>
2020-11-26 06:52:58 +0800

    添加了注释，以及修改了测试目录到正式目录

--- week01/assignment01.py
+++ week01/assignment01.py
@@ -6,19 +6,26 @@ import sys
 
 def log_call():
     today = datetime.date.today().strftime('%Y%m%d')
-    #logfile = '/var/log/python-' + today + '/xxxx.log'
-    logfile = '/Users/davidwu/learn_python/playground/' + today + '/xxxx.log'
-    logdir = '/Users/davidwu/learn_python/playground/' + today
+    logdir = '/var/log/python-' + today
+    
+    # logdir = '/Users/davidwu/learn_python/playground/' + today
+    # 检查日志目录是否存在，如不存在则创建目录
     if not os.path.isdir(logdir):
         try:
             os.mkdir(logdir)
         except OSError as err:
+            print('Create log directory failed: {}'.format(err))
             sys.exit(1)
+    
+    logfile = logdir + '/xxxx.log'
 
+    # 配置日志的文件、级别、格式
     logging.basicConfig(filename=logfile,
                         level=logging.DEBUG,
                         datefmt='%Y-%m-%d %X',
                         format='%(asctime)s %(name)-8s %(levelname)-8s %(message)s')
+    
+    # 日志记录函数被调用的时间
     logging.info("It's called in: {}".format(time.ctime()))
 
 if __name__ == '__main__':
