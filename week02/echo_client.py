ff6165a74c1ff24a9ee94a2b5db38df7fc73f351
David Wu <toffino@aliyun.com>
2020-12-05 11:44:53 +0800

    第一个支持get命令的client版本

--- week02/echo_client.py
+++ week02/echo_client.py
@@ -37,7 +37,6 @@ def echo_client():
 def client_put(data, s):
     elements = data.split()
     # 根据命令的不同参数个数来赋值
-    src_p = Path(elements[1])
     if len(elements) == 2:
         source_file = elements[1]
         target_file = None
@@ -45,7 +44,7 @@ def client_put(data, s):
         source_file = elements[1]
         target_file = elements[2]
     else:
-        print('Error: 错误命令')
+        print('Error: 命令格式错误')
         return
     
     src_p = Path(source_file)
@@ -70,7 +69,66 @@ def client_put(data, s):
         print(response.decode('utf-8'))
 
 def client_get(msg, s):
-    pass
+    elements = mgs.split()
+    if len(elements) == 2:
+        source_file = elements[1]
+        target_file = None
+    elif len(elements) == 3:
+        source_file = elements[1]
+        target_file = elements[2]
+    else:
+        print('Error: 命令格式错误')
+    
+    filename = Path(source_file).name
+    if target_file == None:
+        target_p = Path(__file__).parent.joinpath('recv', filename)
+    elif target_file[-1] == '/':
+        target_p = Path(targt_file).joinpaht(filename)
+    else:
+        target_p = Path(target_file)
+        if target_p.is_dir():
+            target_p = target_p.joinpath(filename)
+    
+    target_dir = target_p.parent
+    filename = target_p.name
+    print(f'File: {filename}, Directory: {target_dir}')
+    try:
+        target_dir.mkdir(parents=True, exist_ok=True)
+        with open(target_p, 'wb') as f:
+            pass
+    except Exception as e:
+        print(f'Error: {e}')
+        return
+    
+    # 发送命令
+    s.sendall('GET {} {}'.format(source_file, target_p).encode('utf-8'))
+
+    # 接受反馈, 按照协议，服务器端应该回传：OK size，或者Error error message
+    rsponse = s.recv(1024)
+    if response.decode('utf-8').startswith('OK'):
+        file_size = int(response.split()[1])
+        try:
+            while open(target_p, 'wb') as f:
+                r_size = 0
+                while r_size < file_size:
+                    buf = s.recv(1024)
+                    r_size += len(buf)
+                    if not buf:
+                        break
+                    else:
+                        f.write(buf)
+        except Exception as e:
+            print(f'Error {e}')
+            s.sendall(f'Error {e}'.encode('utf-8'))
+            return
+        s.sendall('Succeed')
+    else:
+        print(response.decode('utf-8'))
+        
+
+
+
+
 
 if __name__ == '__main__':
     echo_client()
