0e9b490dda00157b488e36c274198ae06358c462
David Wu <toffino@aliyun.com>
2020-12-13 05:41:49 +0800

    用pymysql插入三条数据

--- week03/user_orm_pymysql.py
+++ week03/user_orm_pymysql.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 
+# 用ORM创建表格
 Base = declarative_base()
 
 class User_table(Base):
@@ -19,5 +20,25 @@ class User_table(Base):
 
 dburl = "mysql+pymysql://testuser:WyqWys75$@ec2-3-137-159-11.us-east-2.compute.amazonaws.com:3306/testdb"
 engine = create_engine(dburl, echo=True, encoding='utf-8')
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
+
+# 用pymysql插入三条数据
+db = pymysql.connect("ec2-3-137-159-11.us-east-2.compute.amazonaws.com", "testuser", "WyqWys75$", "testdb")
+try:
+    sql = '''INSERT INTO user_orm (name, age, birthday, gender, education, created_on, updated_on) VALUES (%s, %s, %s, %s, %s, %s, %s)'''
+    values = (
+        ('David Wu', 42, '1978-1-12', 'M', 'Bachelor', datetime.now(), datetime.now()),
+        ('Frank Lin', 30, '1990-10-24', 'M', 'Master', datetime.now(), datetime.now()),
+        ('Ella Wang', 20, '2000-08-24', 'M', 'High School', datetime.now(), datetime.now())
+    )
+    with db.cursor() as cursor:
+        cursor.executemany(sql, values)
+    db.commit()
+except Exception as e:
+    print(f'Insert error: {e}')
+finally:
+    db.close()
+
+
+
 
