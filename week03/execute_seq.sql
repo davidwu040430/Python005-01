3e5d0d007670c2c9861929a312d23691a443e458
David Wu <toffino@aliyun.com>
2020-12-13 07:53:50 +0800

    sql执行顺序

--- week03/execute_seq.sql
+++ week03/execute_seq.sql
@@ -1,3 +1,4 @@
+
 SELECT DISTINCT player_id, player_name, count(*) as num  # 5
 FROM player JOIN team ON player.team_id = team.team_id   # 1
 WHERE height > 1.80                                      # 2
