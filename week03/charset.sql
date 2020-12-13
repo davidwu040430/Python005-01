a400600bfbba1988bf411179a60e6ebbe6828033
David Wu <toffino@aliyun.com>
2020-12-13 11:59:13 +0800

    把问题答案整合到一个文件中，删除其他两个文件

--- week03/charset.sql
+++
@@ -1,9 +0,0 @@
-show variables like '%character%';
-set character_set_server='utf8mb4';
-
-CREATE DATABASE testdb;
-
-CREATE USER 'testuser' IDENTIFIED BY 'testpass8T#';
-GRANT ALL ON testdb.* TO 'testuser'@'%';
-
-
