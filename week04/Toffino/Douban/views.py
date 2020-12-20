91825e287f42a64eaa7e44e0d68556935795dec3
David Wu <toffino@aliyun.com>
2020-12-20 01:17:03 -0800

    配置了url，放入了前段页面

--- week04/Toffino/Douban/views.py
+++ week04/Toffino/Douban/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Review
 
 # Create your views here.
+
+def movie_review(request):
+    # 所有的评论
+    reviews = Review.objects.all()
+    # 评论数量
+    counter = Review.objects.all().count()
+
+    # 评分大于3分的评论
+    return render(request, 'index.html', locals())
