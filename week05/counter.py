6004ff9933d503bb0200112b01988bd0c438bcda
David Wu <toffino@aliyun.com>
2020-12-25 17:47:44 -0800

    题目要求，只传入vedio id

--- week05/counter.py
+++ week05/counter.py
@@ -1,16 +1,17 @@
 import redis
 
 
-def counter(redis_client, vedio_id):
-    result = redis_client.incr(vedio_id)
+def counter(vedio_id):
+    client = redis.Redis(
+        host='ec2-3-137-159-11.us-east-2.compute.amazonaws.com', password='WyqWys75$')
+    result = client.incr(vedio_id)
     return result
 
 
 def main():
-    client = redis.Redis(
-        host='ec2-3-137-159-11.us-east-2.compute.amazonaws.com', password='WyqWys75$')
+
     vedio_id = '10001'
-    count = counter(client, vedio_id)
+    count = counter(vedio_id)
     print('{}: {}'.format(vedio_id, count))
 
 
