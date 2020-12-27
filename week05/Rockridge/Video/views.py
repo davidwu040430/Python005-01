eb0f2d62e1574bad2fa526cc616d78b58cdaefd3
David Wu <toffino@aliyun.com>
2020-12-27 13:10:04 -0800

    修正了逻辑错误，当没有vid参数的时候，报错

--- week05/Rockridge/Video/views.py
+++ week05/Rockridge/Video/views.py
@@ -7,11 +7,13 @@ def video_list(request):
     vlist = [1001, 1002, 1003, 1004, 1005]
     conn = get_redis_connection("default")
     vid = request.GET.get('vid')
-    # 如果有参数vid，如果vid在列表中，vid所在的计数器加1，如果不再列表中，说明是非法参数，给出错误信息，不增加计数器
-    if int(vid) in vlist:
-        conn.incr(vid)
-    else:
-        error = '非法参数，没有vid为"{}"的视频'.format(vid)
+    # 如果有参数vid
+    if vid:
+        # 如果vid在列表中，vid所在的计数器加1，如果不再列表中，说明是非法参数，给出错误信息，不增加计数器
+        if int(vid) in vlist:
+            conn.incr(vid)
+        else:
+            error = '非法参数，没有vid为"{}"的视频'.format(vid)
     # 遍历列表，取出所有的视频的vid和计数器
     counts = []
     for v in vlist:
